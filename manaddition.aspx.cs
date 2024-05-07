d6028c1d6c57662a2a6db5825d437d90d133e144
eeshhwaq <121763046+eeshhwaq@users.noreply.github.com>
2024-05-07 20:27:08 +0500

    Add files via upload
    Admin can add managers and their details to the database

---
+++ manaddition.aspx.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace atm_project
+{
+    public partial class manaddition : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        String connString = ConfigurationManager.ConnectionStrings["ATM_machine"].ConnectionString;
+        private bool ManagerIDExist(string name, string accno, string pin)
+        {
+            string query = "SELECT * FROM [bank_manager] where account_no = @accno ";
+            using (SqlConnection connection = new SqlConnection(connString))
+            {
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@accno", accno);
+                    connection.Open();
+                    var result = command.ExecuteScalar();
+                    if (result != null)
+                    {
+                        return false;
+                    }
+                    else
+                    {
+                        string addQuery = "DECLARE @newmanagerID INT; SET @newmanagerID = (SELECT ISNULL(MAX(managerID), 0) + 1 FROM bank_manager);" + "insert into  [bank_manager] (managerID,manager_name, account_no, PINcode) values(@newmanagerID, @name, @accno, @pin)";
+
+                        using (SqlCommand addCommand = new SqlCommand(addQuery, connection))
+                        {
+                            addCommand.Parameters.AddWithValue("@name", name);
+                            addCommand.Parameters.AddWithValue("@accno", accno);
+                            addCommand.Parameters.AddWithValue("@pin", pin);
+                            addCommand.ExecuteNonQuery();
+                        }
+                        return true;
+                    }
+                }
+            }
+        }
+        protected void confirmadd(object sender, EventArgs e)
+        {
+
+            bool flag = ManagerIDExist(username.Text, accno.Text, pin.Text);
+            if (flag)
+            {
+                Msg.Text = "Manager has been added successfully";
+            }
+            else
+            {
+                Msg.Text = "Manager cannot be added";
+            }
+        }
+
+    }
+}
