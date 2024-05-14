37a196d93aced7dc4832e4aee905b050a6e129b9
DragonBoy53 <ahmedmukhtar024@gmail.com>
2024-05-14 19:17:39 +0500

    Login backend changes for attempts
    number of attempts for login are limited

--- login.aspx.cs
+++ login.aspx.cs
@@ -8,6 +8,12 @@ using System.Data.SqlClient;
 using System.Configuration;
 using System.Data;
 
+struct det
+{
+    public int attempts;
+    public char User;
+    public string ID;
+};
 
 namespace ATMModel
 {
@@ -16,71 +22,166 @@ namespace ATMModel
         public static string userID;
 
         String connString = ConfigurationManager.ConnectionStrings["ATMEntities"].ConnectionString;
-        private char VerifyCredentials(string accountno, string pin)
+        private det VerifyCredentials(string accountno, string pin)
         {
             string query = "SELECT userID FROM [Client_details] WHERE account_no = @AccountNo AND PINcode = @PIN";
-            char userType = 'N';
-
-            using (SqlConnection connection = new SqlConnection(connString))
+            det temp=new det();
+            temp.User = 'N';
+            
+            temp.attempts=getAttempts(accountno, "Client_details");
+            if(temp.attempts<3)
             {
-                using (SqlCommand command = new SqlCommand(query, connection))
+                using (SqlConnection connection = new SqlConnection(connString))
                 {
-                    command.Parameters.AddWithValue("@AccountNo", accountno);
-                    command.Parameters.AddWithValue("@PIN", pin);
-
-                    connection.Open();
-                    var result = command.ExecuteScalar();
-                    if (result != null && result != DBNull.Value)
+                    using (SqlCommand command = new SqlCommand(query, connection))
                     {
-                        userID = result.ToString();
-                        Response.Redirect("User.aspx?userID=" + userID);
-                        userType = 'U';
+                        command.Parameters.AddWithValue("@AccountNo", accountno);
+                        command.Parameters.AddWithValue("@PIN", pin);
 
+                        connection.Open();
+                        var result = command.ExecuteScalar();
+                        if (result != null && result != DBNull.Value)
+                        {
+                            Session["user_accno"] = accountno;
+                            temp.ID= result.ToString();
+                            temp.User= 'U';
+                            resetAttempts(accountno, "Admin");
+                            return temp;
+                        }
+                        else if(result ==null)
+                        {
+                            attemptsincrement(accountno, "Client_details", temp.attempts);
+                        }
                     }
                 }
             }
             query = "SELECT managerID FROM [Manager] WHERE account_no = @AccountNo AND PINcode = @PIN";
 
-            using (SqlConnection connection = new SqlConnection(connString))
+            temp.attempts = getAttempts(accountno, "Manager");
+            if (temp.attempts < 3)
             {
-                using (SqlCommand command = new SqlCommand(query, connection))
+                using (SqlConnection connection = new SqlConnection(connString))
                 {
-                    command.Parameters.AddWithValue("@AccountNo", accountno);
-                    command.Parameters.AddWithValue("@PIN", pin);
-
-                    connection.Open();
-                    var result = command.ExecuteScalar();
-                    if (result != null && result != DBNull.Value)
+                    using (SqlCommand command = new SqlCommand(query, connection))
                     {
-                        userID = result.ToString();
-                        Response.Redirect("manager.aspx?managerID=" + userID);
-                        userType = 'M';
+                        command.Parameters.AddWithValue("@AccountNo", accountno);
+                        command.Parameters.AddWithValue("@PIN", pin);
 
+                        connection.Open();
+                        var result = command.ExecuteScalar();
+                        if (result != null && result != DBNull.Value)
+                        {
+                            temp.ID = result.ToString();
+                            temp.User = 'M';
+                            resetAttempts(accountno, "Manager");
+                            return temp;
+                        }
+                        else if (result == null)
+                        {
+                            attemptsincrement(accountno, "Manager", temp.attempts);
+                        }
                     }
                 }
-
             }
 
             query = "SELECT adminID FROM [Admin] WHERE account_no = @AccountNo AND PINcode = @PIN";
 
-            using (SqlConnection connection = new SqlConnection(connString))
+            temp.attempts = getAttempts(accountno, "Admin");
+            if(temp.attempts < 3)
             {
-                using (SqlCommand command = new SqlCommand(query, connection))
+                using (SqlConnection connection = new SqlConnection(connString))
                 {
-                    command.Parameters.AddWithValue("@AccountNo", accountno);
-                    command.Parameters.AddWithValue("@PIN", pin);
-
-                    connection.Open();
-                    var result = command.ExecuteScalar();
-                    if (result != null && result != DBNull.Value)
+                    using (SqlCommand command = new SqlCommand(query, connection))
                     {
-                        userID = result.ToString();
-                        userType = 'A';
-                        Response.Redirect("admin.aspx?adminID=" + userID);
+                        command.Parameters.AddWithValue("@AccountNo", accountno);
+                        command.Parameters.AddWithValue("@PIN", pin);
+
+                        connection.Open();
+                        var result = command.ExecuteScalar();
+                        if (result != null && result != DBNull.Value)
+                        {
+                            temp.ID = result.ToString();
+                            temp.User = 'A';
+                            resetAttempts(accountno, "Admin");
+                            return temp;
+                        }
+                        else if (result == null)
+                        {
+                            attemptsincrement(accountno, "Admin", temp.attempts);
+                        }
                     }
                 }
             }
-            return userType;
+            return temp;
+        }
+        private int getAttempts(string accountno,string table)
+        {
+            if (accountno != null)
+            {
+                int attempts = 0;
+                string query = "SELECT attempts FROM ["+table+"] WHERE account_no = @AccountNo";
+
+                using (SqlConnection connection = new SqlConnection(connString))
+                {
+                    using (SqlCommand command = new SqlCommand(query, connection))
+                    {
+                        command.Parameters.AddWithValue("@AccountNo", accountno); // Fix: Added @ in parameter name
+                        connection.Open();
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            if (reader.Read())
+                            {
+                                attempts = Convert.ToInt32(reader["attempts"]);
+                            }
+                        }
+                    }
+                }
+
+                return attempts; // Fix: Added return statement
+            }
+
+            return 0; // Fix: Added default return value in case accountno is null
+        }
+        private void attemptsincrement(string accountno, string table,int attempts)
+        {
+            if (attempts < 3)
+            {
+                attempts++;
+                string query = "UPDATE ["+ table +"] SET attempts=@Attempts WHERE account_no=@AccountNo";
+                using (SqlConnection connection = new SqlConnection(connString))
+                {
+                    using (SqlCommand command = new SqlCommand(query, connection))
+                    {
+                        command.Parameters.AddWithValue("@AccountNo", accountno);
+                        command.Parameters.AddWithValue("@Attempts", attempts);
+
+                        connection.Open();
+                        command.ExecuteNonQuery();
+                    }
+                }
+            }
+        }
+        private int resetAttempts(string accountno, string table)
+        {
+            if (accountno != null)
+            {
+                int attempts = 0;
+                string query = "update [" + table + "] set attempts=0 WHERE account_no = @AccountNo";
+
+                using (SqlConnection connection = new SqlConnection(connString))
+                {
+                    using (SqlCommand command = new SqlCommand(query, connection))
+                    {
+                        command.Parameters.AddWithValue("@AccountNo", accountno); // Fix: Added @ in parameter name
+                        connection.Open();
+                        command.ExecuteScalar();
+                    }
+                }
+
+                return attempts; // Fix: Added return statement
+            }
+
+            return 0; // Fix: Added default return value in case accountno is null
         }
 
         protected void btnLogin_Click(object sender, EventArgs e)
@@ -89,23 +190,32 @@ namespace ATMModel
             string username = txtUsername.Text;
             string password = txtPassword.Text;
 
+            det obj;
+
             // Verify credentials
-            char userType = VerifyCredentials(username, password);
+            obj = VerifyCredentials(username, password);
 
             // Redirect based on user type or show error message
-            switch (userType)
+            switch (obj.User)
             {   
                 case 'A':
-                    Response.Redirect("admin.aspx");
+                    Response.Redirect("admin.aspx?adminID=" + obj.ID);
                     break;
                 case 'U':
-                    Response.Redirect("User.aspx");
+                    Response.Redirect("User.aspx?userID=" + obj.ID);
                     break;
                 case 'M':
-                    Response.Redirect("manager.aspx");
+                    Response.Redirect("manager.aspx?managerID=" + obj.ID);
                     break;
                 default:
-                    Msg.Text = "Incorrect username or password.";
+                    if(obj.attempts<2)
+                    {
+                        Msg.Text = "Incorrect username or password.";
+                    }
+                    else
+                    {
+                        Msg.Text = "Account Blocked";
+                    }
                     break;
             }
         }
